8ef8bb0a2805b157267ec51ff8fff5ffac3fda1a
Kira <kira.soba@gmx.de>
2023-02-24 15:56:23 +0100

    Installing nodemon

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/", (req, res) => {
-  res.json("the pilllaaaaarsss ... ")
+  res.json("the pilllaaaaarsss ...")
 })
 
 module.exports = router
