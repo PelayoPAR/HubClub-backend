a0c25db4e02028f633cfe75ec8781ae6728cf595
PelayoPAR <pelayopar@gmail.com>
2023-02-27 20:09:56 +0100

    added route for name param Ironhacker

--- routes/ironhacker.routes.js
+++ routes/ironhacker.routes.js
@@ -3,7 +3,14 @@ const ironhackerRouter = express.Router()
 const ironhackers = require("../data/ironhackers.json")
 
 ironhackerRouter.get("/", (req, res) => {
-res.json(ironhackers)
+  res.json(ironhackers)
+})
+
+ironhackerRouter.get("/:name", (req, res) => {
+  const matchedIronhacker = ironhackers.find(
+    (ironhacker) => ironhacker.slug === req.params.name
+  )
+  res.json(matchedIronhacker)
 })
 
 module.exports = ironhackerRouter
