a3db2f534347aa54d059e428e57e0610758e16a9
Kira <kira.soba@gmx.de>
2023-03-08 21:01:16 +0100

    Fetch projects by ironhacker

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -1,17 +1,31 @@
 const express = require("express")
 const projectRouter = express.Router()
 const projects = require("../data/projects.json")
+const slugify = require("slugify")
 
 projectRouter.get("/", (req, res) => {
   res.json(projects)
 })
 
-projectRouter.get("/:name", (req, res) => {
+projectRouter.get("/:title", (req, res) => {
   const matchedProject = projects.find(
-    (project) => project.slug === req.params.name
+    (project) => project.slug === req.params.title
   )
   res.json(matchedProject)
   // res.json("Hi from the backend of yo ass")
+})
+
+projectRouter.get("/by-owner/:owner", (req, res) => {
+  const matchedProjects = projects.filter((project) => {
+    const found = project.owners.find((owner) => slugify(owner.toLowerCase()) === req.params.owner.toLowerCase())
+
+    if (found){
+      return project
+    }
+
+  })
+
+  res.json(matchedProjects)
 })
 
 module.exports = projectRouter
